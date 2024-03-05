d4d27ad6ddbce687795d44c0f9ed18629fefee0a
Harsha Pramod <harshapramod19980124@gmail.com>
2024-03-05 11:43:40 +0530

    initial commit

---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,48 @@
+// Typescript custom enum for search types (optional)
+export enum SearchType {
+    all = '',
+    movie = 'movie',
+    series = 'series',
+    episode = 'episode',
+    }
+
+    export interface DetailsResult {
+    Genre: string
+    Title: string
+    Year: string
+    Poster: string
+    Plot: string
+    imdbRating: string
+    Director: string
+    Actors: string
+    Website: string
+    }
+    
+    export const useApi = () => {
+    let url = 'http://www.omdbapi.com/'
+    let apiKey = '7edf70f0'
+    // f6b0db6d
+
+    const searchData = async (title: string, type: SearchType): Promise<any> => {
+        const result = await fetch(
+        `${url}?s=${encodeURI(title)}&type=${type}&apikey=${apiKey}`,
+        )
+
+        return result.json()
+        }
+
+        // const getDetails = async (id: string): Promise<DetailsResult> => {
+        //     const result = await fetch('${url}?i=${id}&plot=full&apikey=${apiKey}`)
+        //     return result.json();
+        // }
+        const getDetails = async (id: string): Promise<DetailsResult> => {
+            const result = await fetch(`${url}?i=${id}&plot=full&apikey=${apiKey}`);
+            return result.json();
+        };
+        
+        return {
+            searchData,
+            getDetails,
+            }
+        }   
+        export default useApi
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,65 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import useApi, { SearchType } from "../hooks/useApi";
+import { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
+  const { searchData } = useApi();
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [type, setType] = useState<SearchType>(SearchType.all);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    console.log("SEARCH : ", searchTerm);
+
+    if(searchTerm===''){
+      setResults([]);
+      return
+    }
+    const loadData = async() => {
+      const result = await searchData(searchTerm, type)
+      console.log("`file: HOme.tsx:31 ~ loadData ~ result", result);
+      
+    }
+    loadData()
+
+  }, [searchTerm]);
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle color={"primary"} >My Movie App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonSearchbar
+          value={searchTerm}
+          debounce={300}
+          onIonChange={(e) => setSearchTerm(e.detail.value!)}
+        ></IonSearchbar>
+        <IonItem>
+          <IonLabel>select Searchtype</IonLabel>
+          <IonSelect
+            value={searchTerm}
+            onIonChange={(e) => setType(e.detail.value!)}
+          >
+            <IonSelectOption value={""}>All</IonSelectOption>
+            <IonSelectOption value={"movie"}>Movie</IonSelectOption>
+            <IonSelectOption value={"series"}>Series</IonSelectOption>
+            <IonSelectOption value={"episode"}>Episode</IonSelectOption>
+          </IonSelect>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
